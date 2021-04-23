43738bac09dc156ce9322d853c4d767a97f862d1
久染 <1084350607@qq.com>
2021-04-24 01:57:18 +0800

    chore: init blog directories

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
